dcf5a922b182ebec436c6c12d161c3773e517b75
Vsevolod Romashov <7@7vn.ru>
2016-01-24 12:38:15 +0300

    cards list uses id/password from session

--- src/payter/routes/home.clj
+++ src/payter/routes/home.clj
@@ -6,33 +6,12 @@
             [clojure.java.io :as io]
             [hiccup.form :refer [form-to text-field password-field]]))
 
-(defn form [login password]
-  (form-to {:class "form-horizontal"} [:get "/list"]
-           [:div.form-group
-            [:div.col-sm-offset-2.col-sm-10
-             [:h4 "Get cards list"]]]
-           [:div.form-group
-            [:label.col-sm-2.control-label {:for "login"} "Login"]
-            [:div.col-sm-10
-             (text-field {:class "form-control" :id "login" :placeholder "your login"} "login" login)]]
-           [:div.form-group
-            [:label.col-sm-2.control-label {:for "password"} "Password"]
-            [:div.col-sm-10
-             (password-field {:class "form-control" :id "password" :placeholder "your password"} "password" password)]]
-           [:div.form-group
-            [:div.col-sm-offset-2.col-sm-10
-             [:button.btn.btn-default {:type "submit"} "Get cards list"]]]))
-
-(defn home-page []
-  (layout/base
-   :home
-   (form nil nil)))
-
-(defn get-list-page [login password]
-  (let [list (get-list {:id login :pwd password})]
+(defn home-page [{session :session}]
+  (let [id (:user-id session)
+        password (:user-password session)
+        list (get-list {:id id :pwd password})]
     (layout/base
      :home
-     (form login password)
      [:table.table.table-striped.table-hover
       [:thead
        [:tr
@@ -56,7 +35,5 @@
   (layout/base :about [:h2 "About payter"]))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/list" [login password] (get-list-page login password))
+  (GET "/" request (home-page request))
   (GET "/about" [] (about-page)))
-
