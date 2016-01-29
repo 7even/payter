175304569a030c79c96d61501e6ac04533f0dd87
Vsevolod Romashov <7@7vn.ru>
2016-01-29 11:10:57 +0300

    add POST /add-card

--- src/payter/routes/home.clj
+++ src/payter/routes/home.clj
@@ -1,11 +1,13 @@
 (ns payter.routes.home
   (:require [payter.layout :as layout]
-            [payter.api :refer [get-list]]
-            [compojure.core :refer [defroutes GET]]
+            [payter.api :as api]
+            [compojure.core :refer [defroutes GET POST]]
+            [ring.util.response :refer [redirect]]
             [ring.util.http-response :refer [ok]]
+            [ring.util.anti-forgery :refer [anti-forgery-field]]
             [clojure.java.io :as io]
             [hiccup.element :refer [link-to]]
-            [hiccup.form :refer [form-to text-field password-field label drop-down submit-button]]))
+            [hiccup.form :refer [form-to text-field password-field label drop-down hidden-field submit-button]]))
 
 (defn account-selector [session user-id]
   (if-let [accounts (-> session :accounts keys)]
@@ -28,7 +30,7 @@
    nil
    (account-selector session user-id)
    (if-let [password (get-in session [:accounts user-id])]
-     (let [list (get-list {:id user-id :pwd password})]
+     (let [list (api/get-list {:id user-id :pwd password})]
        [:div
         [:table.table.table-striped.table-hover
          [:thead
@@ -53,6 +55,8 @@
          [:div.modal-dialog.modal-sm
           [:div.modal-content
            (form-to {:class "form-horizontal" :id "form"} [:post "/add-card"]
+                    (hidden-field "user-id" user-id)
+                    (anti-forgery-field)
                     [:div.modal-header
                      [:button.close {:type "button" :data-dismiss "modal"} "&times;"]
                      [:h4.modal-title "Add a new card"]]
@@ -96,10 +100,25 @@
                      (submit-button {:class "btn btn-primary col-sm-12"} "Add")])]]]])
      [:p "You did not choose the account correctly"])))
 
+(defn add-card-page [user-id number holder expiration-date cvv session]
+  (let [password (get-in session [:accounts user-id])
+        [month year] (clojure.string/split expiration-date #"/")
+        result (api/add-card {:id user-id
+                              :pwd password
+                              :number number
+                              :month month
+                              :year year
+                              :holder holder
+                              :cvv cvv})]
+    ;; TODO: handle payture errors
+    (redirect (str "/cards?user-id=" user-id))))
+
 (defn about-page []
   (layout/base :about [:h2 "About payter"]))
 
 (defroutes home-routes
   (GET "/" request (home-page request))
   (GET "/cards" [user-id :as {session :session}] (cards-list-page user-id session))
+  (POST "/add-card" [user-id number holder expiration-date cvv :as {session :session}]
+        (add-card-page user-id number holder expiration-date cvv session))
   (GET "/about" [] (about-page)))
